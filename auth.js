187122ca20ccb959f2e23942e3bd5c4897bbba4d
Rosender yadav <rosenderyadav617@gmail.com>
2025-06-08 17:55:46 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+  res.sendStatus(201);
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.sendStatus(401);
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.sendStatus(401);
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
